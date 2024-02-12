91dbc5c0297fb09d02911a93e704571b1a9cbb9a
chetandatta77 <chetandatta77@gmail.com>
2024-02-12 18:44:38 +0530

    useState changes

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -1,12 +1,36 @@
+import { useState } from 'react';
+
 const Content = () => {
+
+    const [name, setName] = useState('Dave');
+    const [count, setCount] = useState(0);
+
     const handleNameChange = () => {
         const names = ['Bob', 'Kevin', 'Dave'];
         const number = Math.floor(Math.random()*3);
-        return names[number];
+        setName(names[number]);
       }
+
+    const handleClick = ()=>{
+        setCount(count+1);
+        setCount(count+1);
+        console.log(count);
+    }
+
+    const handleClick1 = () => {
+        console.log(count);
+    }
+
+
     return (
         <main>
-            <p>Hello {handleNameChange()}</p>
+            <p onDoubleClick={handleClick}>
+                Hello {name}
+            </p>
+            <button onClick={handleNameChange}>Change Name</button>
+            <button onClick={handleClick}>Increment count</button>
+            <button onClick={handleClick1}>Count state</button>
+
         </main>
     )
 }
